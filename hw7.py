2012145ac796547d85d68ffce4da6df53cb5519c
David Morales <demorales@email.arizona.edu>
2023-04-13 22:27:39 -0700

    cleaned up hw7 saturation

--- hw7.py
+++ hw7.py
@@ -6,39 +6,20 @@ import pandas as pd
 
 # %%
 # define formula for solving saturation
-def theta(hb,b,hm):
-    for i in hm:
-        if i < hb:
-            theta = (hm/hb)**(-1/b)
-            print('ok')
-        else:
-            theta = 1
-    return theta
+def theta(hb, b, hm):
+    hm_length = len(hm)                 # count elements in matric potential list
+    index = np.arange(0, hm_length, 1)  # set an index from zero to N of hm
+    soil_sat = np.zeros(hm_length)      # create an array as long as index
 
-def theta2(hb,b,hm):
-    soil_sat_index = range(len(hm))
-    soil_sat = []
-    for i in soil_sat_index:
-        if hm[i] < hb:
-            theta = (hm/hb)**(-1/b)
-            soil_sat[i] = theta
-        else:
-            soil_sat[i] = 1
-    return soil_sat
-
-def theta3(hb, b, hm):
-    hm_length = len(hm)
-    soil_sat_index = np.arange(0, hm_length, 1)
-    soil_sat = np.zeros(hm_length)
-
-    for i in soil_sat_index:
-        if hm[i] < hb:
+    for i in index:
+        if hm[i] < hb:                  # calculate sat. for hm < air entry pressure
             theta = (hm[i]/hb)**(-1/b)
             soil_sat[i] = theta
-        else:
+        else:                           # set sat. to 1 for values >= air entry pressure
             soil_sat[i] = 1
     return soil_sat
 
+# create dictionary of soil properties
 d = {'texture': ['sand','loamy sand','sandy loam','silt loam','loam','clay loam','sandy clay','silty clay','clay'],
          'porosity' : [.395,.41,.435,.485,.451,.476,.426,.492,.482],
          'Ks' : [1.76e-2,1.56e-2,3.47e-3,7.2e-4,6.95e-4,2.45e-4,2.17e-4,1.03e-4,1.28e-4],
@@ -49,30 +30,24 @@ d = {'texture': ['sand','loamy sand','sandy loam','silt loam','loam','clay loam'
 
 # create dataframe of soil characteristics
 soil_properties = pd.DataFrame(d)
-# create a list of soil names
-soilList = soil_properties.texture.to_list()
-# set soil names as index
-soil_properties.set_index('texture', inplace=True)
+soilList = soil_properties.texture.to_list()        # create a list of soil names
+soil_properties.set_index('texture', inplace=True)  # set soil names as index
 
 # %%
+# create dataframe to recieve sat. calculations
 saturation = pd.DataFrame(np.arange(-1000,0,0.01), columns=['hm'])
 saturation.set_index('hm',inplace=True)
-hm = saturation.index.to_list()
-# %%
-for soil in soil_properties.index.to_list():
-    hb = soil_properties.loc[soil].hb
-    b = soil_properties.loc[soil].b
-    hm = saturation.index.to_list()
-    saturation[soil] = theta3(hb, b, hm)
-# %%
-soil_sat_index = np.arange(0,10,1)
-soil_sat = np.zeros(len(soil_sat_index))
-hb = -12.1
-b = 4.05
 
-for i in soil_sat_index:
-    if hm[i] < hb:
-        theta = (hm[i]/hb)**(-1/b)
-        soil_sat[i] = theta
-    else:
-        soil_sat[i] = 1
+soil_index = soil_properties.index.to_list()
+
+for soil in soil_index:
+    # fix parameter values
+    hb = soil_properties.loc[soil].hb   # fix air entry pressure for soil
+    b = soil_properties.loc[soil].b     # fix pore size distribution for soil
+    hm = saturation.index.to_list()     # create list of matric potential values
+    saturation[soil] = theta(hb, b, hm)
+
+# %%
+def bcModel(Ks, b, theta):
+    K_unsat = Ks*theta**(2*b+3)
+    return K_unsat
